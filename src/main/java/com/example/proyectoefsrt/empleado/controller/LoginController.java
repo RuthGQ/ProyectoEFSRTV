dd07b3dc3e129199b5b0226e20d8fe23b8103781
RuthGQ <rg02192004@gmail.com>
2023-10-17 17:51:58 -0500

    cambiando el request mapping de LoginController

--- src/main/java/com/example/proyectoefsrt/empleado/controller/LoginController.java
+++ src/main/java/com/example/proyectoefsrt/empleado/controller/LoginController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/login")
 @AllArgsConstructor
 public class LoginController {
     private EmpleadoService servicioEmpleado;
