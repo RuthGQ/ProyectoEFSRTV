aba83b3d04f1a07f0465ac3c16d151c64efd5b5a
Daniel <danielgillaguna@gmail.com>
2023-10-22 20:44:22 -0500

    ActualizarCarro

--- src/main/java/com/example/proyectoefsrt/carro/controller/CarroController.java
+++ src/main/java/com/example/proyectoefsrt/carro/controller/CarroController.java
@@ -3,9 +3,14 @@ package com.example.proyectoefsrt.carro.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.proyectoefsrt.carro.entity.Carro;
@@ -17,14 +22,33 @@ public class CarroController {
 	@Autowired
 	private CarroService servicioCarro;
 	
-	@GetMapping(value = {"","/"})
+	@GetMapping("")
 	public List<Carro>listarCarro(){
 		return servicioCarro.listrarCarro();
 	}
 	
-	@GetMapping("/carro/{id}")
+	@GetMapping("/{id}")
 	public Carro obtenerCarro(@PathVariable int id) {
 		return servicioCarro.obtenerCarro(id);
 	}
+	
+	@PostMapping("/insertar")
+	@ResponseBody
+	public Carro insertarCarro(@RequestBody Carro carro) {
+		return servicioCarro.insertarCarro(carro);
+	}
+	 
+	@PutMapping("/actualizar")
+	@ResponseBody
+	public Carro actualizarCarro(@RequestBody Carro carro) {
+		return servicioCarro.actualizarCarro(carro);
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	public void eliminarVendedor(@PathVariable int id) {
+		servicioCarro.eliminarCarro(id);
+	}
+	
 
 }
+
