74822f9021bae5463a75ab51cc53f18ce673f3df
RuthGQ <rg02192004@gmail.com>
2023-11-14 23:45:24 -0500

    agregue al service venta

--- src/main/java/com/example/proyectoefsrt/ProyectoEfsrtApplication.java
+++ src/main/java/com/example/proyectoefsrt/ProyectoEfsrtApplication.java
@@ -2,6 +2,13 @@ package com.example.proyectoefsrt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class ProyectoEfsrtApplication {
@@ -10,4 +17,18 @@ public class ProyectoEfsrtApplication {
         SpringApplication.run(ProyectoEfsrtApplication.class, args);
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200/**","http://localhost:4200","**"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        //configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-type", "x-auth-token"));
+        configuration.setExposedHeaders(List.of("X-Get-Header"));
+
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- src/main/java/com/example/proyectoefsrt/venta/servicio/impl/VentaServiceImpl.java
+++ src/main/java/com/example/proyectoefsrt/venta/servicio/impl/VentaServiceImpl.java
@@ -1,8 +1,15 @@
 package com.example.proyectoefsrt.venta.servicio.impl;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.example.proyectoefsrt.carro.entity.Carro;
+import com.example.proyectoefsrt.carro.repository.ICarroRepository;
+import com.example.proyectoefsrt.detalleVenta.entity.DetalleVenta;
+import com.example.proyectoefsrt.detalleVenta.repository.IDetalleVentaRepository;
 import com.example.proyectoefsrt.venta.dto.VentaTransactionDto;
 import com.example.proyectoefsrt.venta.entity.Venta;
+import com.example.proyectoefsrt.venta.repository.IVentaRepository;
 import com.example.proyectoefsrt.venta.servicio.VentaService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,28 +18,53 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class VentaServiceImpl implements VentaService {
+
+    private IVentaRepository repoVenta;
+    private IDetalleVentaRepository repoDetalle;
+    private ICarroRepository repoCarro;
+
     @Override
     public List<Venta> listarVenta() {
-        return null;
+        return repoVenta.findAll();
     }
 
     @Override
     public Venta obtenerVenta(int id) {
-        return null;
+        return repoVenta.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional(rollbackOn = Exception.class)
     public Venta agregarVenta(VentaTransactionDto venta) throws Exception {
-        return null;
+        Venta ventaEntity = repoVenta.save(venta.getVenta());
+        for (DetalleVenta detalle: venta.getDetalleVenta()){
+            Carro carro = repoCarro.findById(detalle.getCarro().getId()).orElse(null);
+            if (carro.getStock() < 0){
+                throw new Exception();
+            }
+            detalle.setVenta(ventaEntity);
+            repoDetalle.save(detalle);
+            repoCarro.save(carro);
+        }
+        return ventaEntity;
     }
 
     @Override
     public Venta actualizarVenta(Venta venta) {
-        return null;
+        Venta ventaFinded = repoVenta.findById(venta.getId()).orElse(null);
+        if (ventaFinded != null){
+            ventaFinded.setMonto(venta.getMonto());
+            ventaFinded.setFecha((venta.getFecha()));
+            ventaFinded.setNumserie(venta.getNumserie());
+            ventaFinded.setCliente(venta.getCliente());
+            ventaFinded.setEmpleado(venta.getEmpleado());
+            repoVenta.save(ventaFinded);
+        }
+        return ventaFinded;
     }
 
     @Override
     public void eliminarVenta(int id) {
-
+        repoVenta.deleteById(id);
     }
 }
