89b689fc2c203d1ce3787f10a5db38fd22788ec4
RuthGQ <rg02192004@gmail.com>
2023-11-17 18:21:21 -0500

    modificado el aplication

--- src/main/java/com/example/proyectoefsrt/ProyectoEfsrtApplication.java
+++ src/main/java/com/example/proyectoefsrt/ProyectoEfsrtApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,17 +20,12 @@ public class ProyectoEfsrtApplication {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200/**","http://localhost:4200","**"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        //configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-type", "x-auth-token"));
-        configuration.setExposedHeaders(List.of("X-Get-Header"));
-
-        configuration.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("PUT","POST","GET","PATCH","DELETE").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
